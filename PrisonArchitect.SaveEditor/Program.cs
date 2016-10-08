beed0fe6aa975c5aad78978bff46113b8eb3d340
matburton <matburton@gmail.com>
2016-10-09 00:54:51 +0100

    Implemented quick top of app to use code
    But such that it's essentally a no-op on the save file

--- PrisonArchitect.SaveEditor/Program.cs
+++ PrisonArchitect.SaveEditor/Program.cs
@@ -1,10 +1,32 @@
 ﻿
+using System.IO;
+
+using PrisonArchitect.SaveEditor.Converters;
+using PrisonArchitect.SaveEditor.Mutator;
+
 namespace PrisonArchitect.SaveEditor
 {
     public static class Program
     {
-        public static void Main()
+        public static int Main(string[] arguments)
         {
+            if (arguments.Length != 2) return 1;
+
+            var saveDocumentString = File.ReadAllText(arguments[0]);
+
+            var saveDocumentConverter = new SaveDocumentConverter();
+
+            var saveDocument = saveDocumentConverter.Parse(saveDocumentString);
+
+            var saveDocumentMutator = new MapShiftingSaveDocumentMutator(0, 0);
+
+            saveDocument = saveDocumentMutator.Mutate(saveDocument);
+
+            saveDocumentString = saveDocumentConverter.Persist(saveDocument);
+
+            File.WriteAllText(arguments[1], saveDocumentString);
+
+            return 0;
         }
     }
 }
